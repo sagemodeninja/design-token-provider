5a3567422e56358017924a899134aa8312e26c83
sagemodeninja <contact@garyantier.com>
2023-07-25 10:31:51 +0800

    feat: observer and get value

--- src/@types/design-token-provider.d.ts
+++ src/@types/design-token-provider.d.ts
@@ -1,8 +1,19 @@
 declare module '@sagemodeninja/design-token-provider' {
+    type DesignTokenTarget = HTMLElement | Document;
+
+    type DesignTokenObserverCallback = (oldValue: any, newValue: any) => void;
+
     export class DesignToken<T> {
         constructor(name: string);
-        setDefault(value: T): DesignToken<any>;
-        setDefault(value: T, override: boolean): DesignToken<any>;
-        setValueFor(target: HTMLElement, value: T): DesignToken<any>;
+        setDefault(value: T): this;
+        setDefault(value: T, override: boolean): this;
+        setValueFor(target: DesignTokenTarget, value: T): this;
+        getDefaultValue(): T;
+        getValueFor(target: DesignTokenTarget): T;
+        subscribe(callback: DesignTokenObserverCallback): this;
+        subscribeFor(
+            target: DesignTokenTarget,
+            callback: DesignTokenObserverCallback
+        ): this;
     }
 }
--- src/design-token-provider.ts
+++ src/design-token-provider.ts
@@ -1,19 +1,19 @@
 type DesignTokenTarget = HTMLElement | Document;
 
+type DesignTokenObserverCallback = (oldValue: any, newValue: any) => void;
+
 export class DesignTokenNode {
     private static _nodes: Map<DesignTokenTarget, DesignTokenNode> = new Map();
 
     private _target: DesignTokenTarget;
-    private _values: Map<DesignToken<any>, any> = new Map();
+    private _values: Map<string, any> = new Map();
     private _debounceTimer: number;
-
-    public static defaultNode: DesignTokenNode;
 
     constructor(target: DesignTokenTarget) {
         this._target = target;
     }
 
-    public static getOrCreate(target: HTMLElement): DesignTokenNode {
+    public static getOrCreate(target: DesignTokenTarget): DesignTokenNode {
         if (DesignTokenNode._nodes.has(target))
             return DesignTokenNode._nodes.get(target);
 
@@ -28,35 +28,38 @@ export class DesignTokenNode {
         value: any,
         override: boolean
     ) {
-        if (!override && this._values.has(token)) return;
+        if (!override && this._values.has(token.name)) return;
 
-        this._values.delete(token);
-        this._values.set(token, value);
+        const oldValue = this._values.get(token.name);
+        this._values.set(token.name, value);
+
+        DesignTokenObserver.notify(token, this._target, oldValue, value);
         this.applyTokens();
     }
 
+    public getTokenValue(token: DesignToken<any>) {
+        console.log(this, this._values, token);
+        return this._values.get(token.name);
+    }
+
     private applyTokens() {
-        if (this._debounceTimer) {
-            window.clearTimeout(this._debounceTimer);
-            this._debounceTimer = null;
-        }
+        if (this._debounceTimer) window.clearTimeout(this._debounceTimer);
 
         const target = this._target;
 
         this._debounceTimer = window.setTimeout(() => {
+            this._debounceTimer = null;
+
             if (target instanceof HTMLElement) {
                 this._values.forEach((value, token) => {
-                    target.style.setProperty(
-                        `--${token.name}`,
-                        value.toString()
-                    );
+                    target.style.setProperty(`--${token}`, value.toString());
                 });
                 return;
             }
 
             let rulesStr = '';
             this._values.forEach((value, token) => {
-                rulesStr += `--${token.name}: ${value};`;
+                rulesStr += `--${token}: ${value};`;
             });
 
             const styleSheet = target.styleSheets[0];
@@ -75,6 +78,40 @@ export class DesignTokenNode {
     }
 }
 
+export class DesignTokenObserver {
+    private static _observers: Map<
+        string,
+        Map<DesignTokenTarget, DesignTokenObserverCallback>
+    > = new Map();
+
+    public static observe(
+        token: DesignToken<any>,
+        target: DesignTokenTarget,
+        callback: DesignTokenObserverCallback
+    ) {
+        let innerMap = DesignTokenObserver._observers.get(token.name);
+
+        if (!innerMap) {
+            innerMap = new Map();
+            DesignTokenObserver._observers.set(token.name, innerMap);
+        }
+
+        innerMap.set(target, callback);
+    }
+
+    public static notify(
+        token: DesignToken<any>,
+        target: DesignTokenTarget,
+        oldValue: any,
+        newValue: any
+    ) {
+        const innerMap = DesignTokenObserver._observers.get(token.name);
+        const callback = innerMap?.get(target);
+
+        if (callback) callback(oldValue, newValue);
+    }
+}
+
 export class DesignToken<T> {
     public static rootIndex: number;
 
@@ -82,18 +119,39 @@ export class DesignToken<T> {
 
     constructor(name: string) {
         this.name = name;
-
-        if (!DesignTokenNode.defaultNode)
-            DesignTokenNode.defaultNode = new DesignTokenNode(document);
     }
 
     setDefault(value: T, override: boolean = true): this {
-        DesignTokenNode.defaultNode.setTokenValue(this, value, override);
+        DesignTokenNode.getOrCreate(document).setTokenValue(
+            this,
+            value,
+            override
+        );
         return this;
     }
 
     setValueFor(target: HTMLElement, value: T): this {
         DesignTokenNode.getOrCreate(target).setTokenValue(this, value, true);
+        return this;
+    }
+
+    getDefaultValue() {
+        return this.getValueFor(document);
+    }
+
+    getValueFor(target: DesignTokenTarget) {
+        return DesignTokenNode.getOrCreate(target).getTokenValue(this);
+    }
+
+    subscribe(callback: DesignTokenObserverCallback) {
+        return this.subscribeFor(document, callback);
+    }
+
+    subscribeFor(
+        target: DesignTokenTarget,
+        callback: DesignTokenObserverCallback
+    ) {
+        DesignTokenObserver.observe(this, target ?? document, callback);
         return this;
     }
 }
